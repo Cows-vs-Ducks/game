91bbba43ba483ff7dcc043ce2bd3081077d142cc
WildCat21 <85885539+WildCat21@users.noreply.github.com>
2021-07-12 18:42:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import sys
 class registrer(QWidget): 
     def __init__(self, parent=None): 
         super().__init__(parent)
-        app = QtWidgets.QApplication(sys.argv) 
-        reg = registrer()
-        reg.resize(1000, 1000) 
-        reg.show()
-        sys.exit(app.exec_())
+        
+app = QApplication(sys.argv) 
+reg = registrer()
+reg.resize(app.desktop()) 
+reg.show()
+sys.exit(app.exec_())
