45a465611545b7bc555ec57c4a9ca0e1719bafd6
WildCat21 <85885539+WildCat21@users.noreply.github.com>
2021-07-12 18:51:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,21 @@ class registrer(QWidget):
     def __init__(self, parent=None): 
         super().__init__(parent)
         
-        nm = QLineEdit()
+        l1 = QHBoxLayout()
+        w1 = QHBoxLayout()
+        l2 = QHBoxLayout()
+        w2 = QHBoxLayout()
+        l3 = QHBoxLayout()
+        w3 = QHBoxLayout()
+        l4 = QHBoxLayout()
+        w4 = QHBoxLayout()
+        
+        vn = QLineEdit()
+        nn = QLineEdit()
+        ag = QLineEdit()
+        gb = QLineEdit()
+        
+        
         
 app = QApplication(sys.argv) 
 reg = registrer()
