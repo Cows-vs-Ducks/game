a17eb8d64865d729bb8117c4ba4f499a2eeca643
WildCat21 <85885539+WildCat21@users.noreply.github.com>
2021-08-03 16:39:50 +0200

    Display moneten

--- ava.py
+++ ava.py
@@ -91,6 +91,17 @@ class market(QWidget):
                
         ly = QVBoxLayout()
         
+        datei = open("user.cvd", "r")
+        uss = datei.read()
+        datei.close()
+        deta = Deta("a0nx7pgk_CAsXSD5UjJsWT8xj9nPSAb14xduJ1fUR")
+        users = deta.Base("user")
+        user = users.get(uss) # the user
+        mon = user["moneten"]
+        
+        self.monn = QLabel(str(mon) + " m")
+        ly.addWidget(self.monn)
+        
         waff1 = QLabel("Waffen")
         ly.addWidget(waff1)
         
